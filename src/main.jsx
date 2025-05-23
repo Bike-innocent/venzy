46155037bae1feee6a163f71dc47e2f4cfd7fc83
bike-innocent <onyemaobichibuikeinnocent.com@gmail.com>
2025-05-23 16:00:26 +0100

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Your global styles (Tailwind base)
+import './index.css';
+
+// Template styles (from src/assets/css/)
+import './assets/css/bootstrap.min.css';
+import './assets/css/LineIcons.3.0.css';
+import './assets/css/tiny-slider.css';
+import './assets/css/glightbox.min.css';
+import './assets/css/main.css';
+// import './assets/js/main.js';
+
+
+
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../component/Header'
+import Hero from './mini-component/Hero'
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <Hero/>
+        </>
+    )
+}
+
+export default Home
